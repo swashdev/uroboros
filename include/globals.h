8858d24c9e159da83c98c3fd540c388cff5d0e5a
swashberry <swashdev@pm.me>
2022-09-14 18:34:07 -0500

    Initialize globals.h
    The globals.h file is used to store global variables and constants.

---
+++ include/globals.h
@@ -0,0 +1,36 @@
+// This is public domain software.  Feel free to do whatever you want with it
+// so long as you don't hold me liable for any damages - there is no warranty.
+//
+// If you want to pay me back for it, you can do so by taking ten minutes or
+// more out of each day to remind yourself that you are valuable and your life
+// is worth living, and treating yourself like someone worth taking care of.
+// This software is already free; now free yourself.
+
+
+#ifndef INCLUDE_GLOBALS_H
+# define INCLUDE_GLOBALS_H
+
+
+// This header file is used for global variables and constants.
+
+
+// The version number for the program.  An effort is made to follow the
+// semantic versioning standard 2.0.0 <https://semver.org/spec/v2.0.0.html>
+// Dev builds, indicated by a version number ending with "-dev.%d", represent
+// prerelease versions, akin to alphas or betas in programs that have actual
+// development teams and production quality.
+# define VERSION_MAJOR 0
+# define VERSION_MINOR 0
+# define VERSION_PATCH 0
+# define VERSION_BUILD 0
+
+
+// Constants used to represent directions.
+# define INVALID  0
+# define UP      -1
+# define DOWN     1
+# define LEFT    -2
+# define RIGHT    2
+
+
+#endif
