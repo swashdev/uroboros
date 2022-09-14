e120a3bd90d63610f44059617f0829ce2c3ba471
swashberry <swashdev@pm.me>
2022-09-14 17:19:48 -0500

    Sketch snake segments.
    This commit defines the struct `segment`, which is a node in a linked
    list used to represent the snake.

---
+++ include/segment.h
@@ -0,0 +1,50 @@
+// This is public domain software.  Feel free to do whatever you want with it
+// so long as you don't hold me liable for any damages - there is no warranty.
+//
+// If you want to pay me back for it, you can do so by taking ten minutes or
+// more out of each day to remind yourself that you are valuable and your life
+// is worth living, and treating yourself like someone worth taking care of.
+// This software is already free; now free yourself.
+
+
+// The segments of the snake are stored in a linked list.  Each segment stores
+// its own coordinates to make it easier to move them independently.
+
+
+#ifndef INCLUDE_SEGMENT_H
+# define INCLUDE_SEGMENT_H
+
+
+typedef struct segment segment;
+struct segment
+{
+    int x, y;
+
+    segment *next;
+};
+
+
+// Returns whether or not the given `node` is the final node in the list.
+char is_tail( segment *node );
+
+
+// Gets the `next` segment in the list from the `current` segment.
+segment *get_next_segment( segment *current );
+
+
+// Moves the `current` segment to the given `x` and `y` coordinates, and
+// recursively moves all of the segments following it.
+void move_segment( segment *current, int x, int y );
+
+
+// Adds a new segment with the given `x` and `y` values to the `current`
+// segment.
+void add_segment( segment *current, int x, int y );
+
+
+// Recursively frees up memory allocated for all of the segments in the list,
+// starting with the `current` one.
+void destroy_segment( segment *current );
+
+
+#endif
