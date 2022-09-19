d346555fc62c750576c348b44a13f2e4a2502d3c
swashberry <swashdev@pm.me>
2022-09-19 17:54:29 -0500

    Bump version number to 0.1.0
    \o/

--- include/globals.h
+++ include/globals.h
@@ -22,7 +22,7 @@
 # define VERSION_MAJOR 0
 # define VERSION_MINOR 1
 # define VERSION_PATCH 0
-# define VERSION_BUILD 3
+# define VERSION_BUILD 0
 
 
 // Enable this flag to build a debug version of the program.  Debug versions
