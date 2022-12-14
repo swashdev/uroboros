39e272ea74ddd2fd475373ed5bda0a5f97211224
swashberry <swashdev@pm.me>
2022-12-14 10:54:59 -0600

    Finalize version 0.3.0

--- include/globals.h
+++ include/globals.h
@@ -29,7 +29,7 @@
 # define VERSION_MAJOR 0
 # define VERSION_MINOR 3
 # define VERSION_PATCH 0
-# define VERSION_BUILD 3
+# define VERSION_BUILD 0
 
 
 // The version number expressed as a hexadecimal number, with each of MAJOR,
